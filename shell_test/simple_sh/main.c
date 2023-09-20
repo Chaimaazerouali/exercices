d96e2da3df50a4d949507baabb6660733dc27413
chaimaazerouali <zeroualiiichaimaa@gmail.com>
2023-09-20 21:23:00 +0100

    ma

--- shell_test/simple_sh/main.c
+++ shell_test/simple_sh/main.c
@@ -2,44 +2,44 @@
 
 /**
  * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * @c: arg count
+ * @v: arg vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
+int main(int c, char **v)
 {
 my_info_t info[] = { MY_INFO_INIT };
-int fd = 2;
+int f = 2;
 
 asm ("mov %1, %0\n\t"
 "add $3, %0"
-: "=r" (fd)
-: "r" (fd));
+: "=r" (f)
+: "r" (f));
 
-if (ac == 2)
+if (c == 2)
 {
-fd = open(av[1], O_RDONLY);
-if (fd == -1)
+f = open(v[1], O_RDONLY);
+if (f == -1)
 {
 if (errno == EACCES)
 exit(126);
 if (errno == ENOENT)
 {
-my_eputs(av[0]);
+my_eputs(v[0]);
 my_eputs(": 0: Can't open ");
-my_eputs(av[1]);
+my_eputs(v[1]);
 my_eputchar('\n');
 my_eputchar(MY_BUF_FLUSH);
 exit(127);
 }
 return (EXIT_FAILURE);
 }
-info->readfd = fd;
+info->readfd = f;
 }
 populate_my_env_list(info);
 read_my_history(info);
-my_hsh(info, av);
+my_hsh(info, v);
 return (EXIT_SUCCESS);
 }
 
